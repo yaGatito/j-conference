5395e1b2e564761a6a9ee8067336a0c65272a351
Nikita <junior.lazovskyi@gmail.com>
2022-07-20 16:23:22 +0300

    Minor changes like as cleaning, renaming, refactoring

--- src/main/java/com/epam/jconference/repository/impl/EventListenerRepositoryImpl.java
+++ src/main/java/com/epam/jconference/repository/impl/EventListenerRepositoryImpl.java
@@ -2,13 +2,13 @@ package com.epam.jconference.repository.impl;
 
 import com.epam.jconference.model.EventListener;
 import com.epam.jconference.repository.EventListenerRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-@Repository
+@Component
 public class EventListenerRepositoryImpl implements EventListenerRepository {
 
     @PersistenceContext
@@ -17,23 +17,23 @@ public class EventListenerRepositoryImpl implements EventListenerRepository {
     @Transactional
     public void add(EventListener eventListener) {
         entityManager.createNativeQuery("INSERT INTO event_listeners (listeners_id, event_id) VALUES (?1, ?2);")
-                .setParameter(1, eventListener.getListener().getId())
-                .setParameter(2, eventListener.getEvent().getId())
-                .executeUpdate();
+                     .setParameter(1, eventListener.getListener().getId())
+                     .setParameter(2, eventListener.getEvent().getId())
+                     .executeUpdate();
     }
 
     public boolean exists(EventListener eventListener) {
         return entityManager.createNativeQuery("SELECT * FROM event_listeners WHERE event_id = ? AND listeners_id = ?")
-                .setParameter(1, eventListener.getEvent().getId())
-                .setParameter(2, eventListener.getListener().getId())
-                .getResultList().size() > 0;
+                            .setParameter(1, eventListener.getEvent().getId())
+                            .setParameter(2, eventListener.getListener().getId())
+                            .getResultList().size() > 0;
     }
 
     @Transactional
     public void remove(EventListener eventListener) {
         entityManager.createNativeQuery("DELETE FROM event_listeners WHERE listeners_id = ?1 AND event_id =?2;")
-                .setParameter(1, eventListener.getListener().getId())
-                .setParameter(2, eventListener.getEvent().getId())
-                .executeUpdate();
+                     .setParameter(1, eventListener.getListener().getId())
+                     .setParameter(2, eventListener.getEvent().getId())
+                     .executeUpdate();
     }
 }
