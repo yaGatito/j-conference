f38c7738e75e38512d8e65b9234dfdaa557b91d9
Nikita <junior.lazovskyi@gmail.com>
2022-07-20 16:34:22 +0300

    Tag business logic

--- src/main/java/com/epam/jconference/repository/TagRepository.java
+++ src/main/java/com/epam/jconference/repository/TagRepository.java
@@ -4,10 +4,7 @@ import com.epam.jconference.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    @Override
-    List<Tag> findAllById(Iterable<Long> longs);
+    boolean existsByName(String name);
 }
--- src/main/java/com/epam/jconference/service/TagService.java
+++ src/main/java/com/epam/jconference/service/TagService.java
@@ -1,20 +1,16 @@
 package com.epam.jconference.service;
 
 import com.epam.jconference.dto.TagDto;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Map;
 
 public interface TagService {
     TagDto create(TagDto tag);
 
     TagDto getById(Long id);
 
-    TagDto update(TagDto tag);
-
-    TagDto addTranslations(Long id, Map<String, String> translations);
+    ResponseEntity<Void> deleteById(Long id);
 
     List<TagDto> findAll();
-
-    List<TagDto> findAllByID(List<Long> ids);
 }
